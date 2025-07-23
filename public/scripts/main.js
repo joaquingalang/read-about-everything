a403c5da81416fc1f5c59b4917e1363f81177381
joaquingalang <galangjoaquin@gmail.com>
2025-07-23 18:32:04 +0800

    Update: changed symbol

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,13 @@
-    function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function renderHeroTitle() {
-    const fullTitle = "read about everything";
+    const fullTitle = "read about everything.";
     let title = "";
 
     for (let i = 0; i < 5; i++) {
-        $(".hero-title").text("_");
+        $(".hero-title").text("|");
         await sleep(250);
         $(".hero-title").text("");
         await sleep(250);
@@ -15,13 +15,13 @@ async function renderHeroTitle() {
 
     for (let i = 0; i < fullTitle.length; i++) {
         title += fullTitle[i];
-        $(".hero-title").text(title + "_")
+        $(".hero-title").text(title + "|")
         await sleep(80);
     }
 
-    $(".hero-title").text(fullTitle + "_");
-    await sleep(250);
-    $(".hero-title").text(fullTitle + ".");
+    $(".hero-title").text(fullTitle + "|");
+    await sleep(80);
+    $(".hero-title").text(fullTitle);
 }
 
 $(window).on("load", renderHeroTitle);
