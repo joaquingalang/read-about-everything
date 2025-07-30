d0e8291818cea8651ce3ccce1e5fd02c7803823d
joaquingalang <galangjoaquin@gmail.com>
2025-07-30 22:29:16 +0800

    Update: store blog posts

--- server.js
+++ server.js
@@ -2,24 +2,38 @@ import express from "express";
 import { stat } from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import BlogPost from "./public/scripts/BlogPost.js";
 
 const app = express();
 const port = 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// var blogPosts = [];
+var blogPosts = [
+    
+];
 
-// var blog = {
-//     title: "My First Blog",
-//     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-//     author: "Joaquin Galang",
-// };
+createPost();
+createPost();
+
+function createPost() {
+    let newPost = new BlogPost(
+        "My First Post",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Joaquin Galang",
+        "https://www.w3schools.com/css/paris.jpg",
+    );
+
+    blogPosts.push(newPost);
+}
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    const data = {
+        posts: blogPosts,
+    };
+    res.render("index.ejs", data);
 });
 
 app.post("/blog", (req, res) => {
